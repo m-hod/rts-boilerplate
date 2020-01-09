b01b983b15dfdfb3c11c70dd4651e08ad9e4be98
m-hodges <50115036+m-hodges@users.noreply.github.com>
2020-01-09 21:32:29 +1300

    add webpack, babel, react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            Welcome to the react-typescript-sass boilerplate
+        </div>
+    )
+}
+
+export default App
