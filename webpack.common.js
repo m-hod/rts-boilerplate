97d6f5ef8495b0fde99cfc9e1ad9e303cb6d8c69
m-hodges <50115036+m-hodges@users.noreply.github.com>
2020-02-03 00:29:57 +1300

    prune old files

--- webpack.common.js
+++ webpack.common.js
@@ -13,7 +13,6 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist/')
-        // publicPath: '/dist/'
     },
     module: {
         rules: [
--- webpack.dev.js
+++ webpack.dev.js
@@ -8,7 +8,6 @@ module.exports = merge(common, {
     devServer: {
         contentBase: path.join(__dirname, 'public/'),
         port: 3000,
-        // publicPath: 'http://localhost:3000/',
-        hotOnly: true
+        hot: true
     }
 })
